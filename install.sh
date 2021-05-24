9c1815dd89a5c766bab36efe0852c13a540be400
Roberto Arcomano <bertolinux@gmail.com>
2021-05-24 20:03:18 +0200

    new branch

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ FULL_IMAGE=robertoarcomano/$IMAGE
 # 1. Download git repository
 git clone https://github.com/robertoarcomano/DockerLemp.git /tmp/DockerLemp
 
-# 2. Download Dockerfile and create image
+# 2. Download Dockerfile_web and create image
 docker rmi -f $FULL_IMAGE
 docker build -t $FULL_IMAGE /tmp/DockerLemp
 
@@ -17,5 +17,5 @@ docker create -p 81:80 --name "$IMAGE" $FULL_IMAGE
 # 4. Start the container
 docker start $IMAGE
 
-# 5. Delete /tmp/Dockerfile
+# 5. Delete /tmp/Dockerfile_web
 rm -rf /tmp/DockerLemp
--- install_image_only.sh
+++ install_image_only.sh
@@ -4,9 +4,9 @@
 # 1. Download git repository
 git clone https://github.com/robertoarcomano/DockerLemp.git /tmp/DockerLemp
 
-# 2. Download Dockerfile and create image
+# 2. Download Dockerfile_web and create image
 docker rmi -f robertoarcomano/dockerlemp
 docker build -t robertoarcomano/dockerlemp /tmp/DockerLemp
 
-# 3. Delete /tmp/Dockerfile
+# 3. Delete /tmp/Dockerfile_web
 rm -rf /tmp/DockerLemp
