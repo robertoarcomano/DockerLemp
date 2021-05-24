f0d17cf3c0b93404bd285741d0f46bcc28ebc8bb
Roberto Arcomano <bertolinux@gmail.com>
2021-05-24 21:52:49 +0200

    Fixed mistakes 6

--- install.sh
+++ install.sh
@@ -11,14 +11,14 @@ TMP_DIR=/tmp/DockerLemp
 git clone -b $BRANCH https://github.com/robertoarcomano/DockerLemp.git $TMP_DIR
 
 # 2. Download Dockerfile_web and create image
-docker rmi -f $IMAGE_WEB
-docker rmi -f $IMAGE_DB
+docker rmi -f $FULL_IMAGE_WEB
+docker rmi -f $FULL_IMAGE_DB
 docker build -t $FULL_IMAGE_WEB -f $TMP_DIR/Dockerfile_web $TMP_DIR
 docker build -t $FULL_IMAGE_DB -f $TMP_DIR/Dockerfile_db $TMP_DIR
 
 # 3. Create the container from the image
-docker rm -f $FULL_IMAGE_WEB
-docker rm -f $FULL_IMAGE_DB
+docker rm -f $IMAGE_WEB
+docker rm -f $IMAGE_DB
 docker create -p 81:80 --name "$IMAGE_WEB" $FULL_IMAGE_WEB
 docker create -p 3307:3306 --name "$IMAGE_DB" $FULL_IMAGE_DB
 
